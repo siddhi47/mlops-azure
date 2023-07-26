8d5a3871ab9ac1e30363bb91744f9691273e87e1
Siddhi <siddhi.47.skb@gmail.com>
2023-07-25 19:41:19 -0500

    refactored code

--- src/model/train.py
+++ src/model/train.py
@@ -5,7 +5,7 @@ import glob
 import os
 
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import mlflow
 
@@ -47,9 +47,11 @@ def split_data(df):
         ].values,
         df["Diabetic"].values,
     )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
+def train_model(reg_rate, X_train,y_train):
     # train model
     LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
 
